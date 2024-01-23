0c82853630c1bc9f4b6097070f3113c98bd68a2c
Ananda Fadhilah <perkasa.putra1404@gmail.com>
2024-01-23 23:55:28 +0800

    Products controller has been updated!

--- src/controller/products.js
+++ src/controller/products.js
@@ -16,7 +16,7 @@ const {
     getAllProduct: async (req, res) => {
       try {
         const page = Number(req.query.page) || 1;
-        const limit = Number(req.query.limit) || 10;
+        const limit = Number(req.query.limit) || 20;
         const offset = (page - 1) * limit;
         const sortby = req.query.sortby || "product_name";
         const sort = req.query.sort || "ASC";
